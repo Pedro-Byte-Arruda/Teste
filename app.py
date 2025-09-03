a53b740e3da567004c4cc22aff6a4e83ac761f0e
Pedro-Byte-Arruda <pedro.alves.fecap@gmail.com>
2025-09-03 14:14:18 -0300

    refactor: alter teste

--- app.py
+++ app.py
@@ -9,60 +9,40 @@ def home():
 #Questions HTML
 
 @app.route('/QuestHTML1')
-def home():
+def QuestHTML1():
     return render_template('QuestHTML1.html')
 
 @app.route('/QuestHTML2')
-def home():
+def QuestHTML2():
     return render_template('QuestHTML2.html')
 
 @app.route('/QuestHTML3')
-def home():
+def QuestHTML3():
     return render_template('QuestHTML3.html')
 
 @app.route('/QuestHTML4')
-def home():
+def QuestHTML4():
     return render_template('QuestHTML4.html')
 
 @app.route('/QuestHTML5')
-def home():
+def QuestHTML5():
     return render_template('QuestHTML5.html')
 
 @app.route('/QuestHTML6')
-def home():
+def QuestHTML6():
     return render_template('QuestHTML6.html')
 
 @app.route('/QuestHTML7')
-def home():
+def QuestHTML7():
     return render_template('QuestHTML7.html')
 
 @app.route('/QuestHTML8')
-def home():
+def QuestHTML8():
     return render_template('QuestHTML8.html')
 
 @app.route('/QuestHTML9')
-def home():
+def QuestHTML9():
     return render_template('QuestHTML9.html')
-
-@app.route('/Quest1')
-def home():
-    return render_template('Quest1.html')
-
-@app.route('/Quest1')
-def home():
-    return render_template('Quest1.html')
-
-@app.route('/Quest1')
-def home():
-    return render_template('Quest1.html')
-
-@app.route('/Quest1')
-def home():
-    return render_template('Quest1.html')
-
-@app.route('/Quest1')
-def home():
-    return render_template('Quest1.html')
 
 if __name__ == '__main__':
     app.run(debug = True)
